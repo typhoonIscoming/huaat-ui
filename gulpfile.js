ca4a2255ad166dfe39bb005ee645b7784c4249a1
typhoonIscoming <850525344@qq.com>
2021-11-02 18:39:00 +0800

    mod: 配置

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,32 @@
 const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
-const less = require('gulp-less');
+const sass = require('gulp-sass')(require('sass'));
 const rename = require('gulp-rename');
-const autoprefixer = require('gulp-autoprefixer');
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer');
 const components = require('./build/components.json');
 
 function buildCss(cb) {
-    gulp.src('../styles/index.scss')
-        .pipe(less())
-        .pipe(autoprefixer())
+    gulp.src('./styles/index.scss')
+        .pipe(sass())
+        .pipe(postcss([autoprefixer()]))
         .pipe(cleanCSS())
         .pipe(rename('index.css'))
-        .pipe(gulp.dest('../lib/styles'));
+        .pipe(gulp.dest('./lib/styles'));
     cb()
 }
 
 function buildSeperateCss(cb) {
     Object.keys(components).forEach(compName => {
-        gulp.src(`../styles/${compName}.less`)
-            .pipe(less())
-            .pipe(autoprefixer())
+        gulp.src(`./styles/${compName}.scss`)
+            .pipe(sass())
+            .pipe(postcss([autoprefixer()]))
             .pipe(cleanCSS())
             .pipe(rename(`${compName}.css`))
-            .pipe(gulp.dest('../lib/styles'));
+            .pipe(gulp.dest('./lib/styles'));
     })
 
     cb()
 }
 
-exports.default = gulp.series(buildCss, buildSeperateCss);
+exports.default = gulp.series(buildCss);
